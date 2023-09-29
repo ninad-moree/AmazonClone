02d311769ac600177d69fe80bc304c32a0f9ddac
Ninad More <ninadyogeshmore@gmail.com>
2023-09-29 12:09:35 +0530

    Auth Ui Done

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,34 @@
+import 'package:amazon/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(double.infinity, 50),
+        backgroundColor: GlobalVariables.secondaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/router.dart
@@ -0,0 +1,20 @@
+import 'package:amazon/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routename:
+      return MaterialPageRoute(
+        builder: (_) => const AuthScreen(),
+      );
+    default:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const Scaffold(
+          body: Center(
+            child: Text('The Screen doesn\'t exist'),
+          ),
+        ),
+      );
+  }
+}
